5c07f1560eef561805a8568d433b4d48113acd8c
aashish-nomad <aashish.nomad@gmail.com>
2025-08-30 20:33:16 +0545

    Uses useCallback for a function that is passed as a dependency value to avoid infinite re renders

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
@@ -47,7 +47,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -55,7 +55,8 @@ function App() {
 
     const storeIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
     localStorage.setItem('selectedPlaces', JSON.stringify(storeIds.filter(id => id != selectedPlace.current)));
-  }
+  }, []);
+
 
   return (
     <>
